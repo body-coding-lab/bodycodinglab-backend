79e1adf47d1b1b594ad13d1880d194c290f7c53d
bbang-2 <yys799@nate.com>
2025-07-02 13:01:09 +0900

    feature: responsecode 구현

---
+++ bodycodinglab-backend/src/test/java/com/example/bodycodinglabbackend/BodycodinglabBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bodycodinglabbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodycodinglabBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
