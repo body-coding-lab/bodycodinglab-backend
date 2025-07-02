42ff313cd8b9cb79dd8300d10a5dda8864e28718
mijinn0 <pmj_0818@naver.com>
2025-07-02 14:23:10 +0900

    refactor: v2 API 경로 리팩토링

--- src/main/java/com/bcl/fitmate/backend/common/constants/ApiMappingPattern.java
+++ src/main/java/com/bcl/fitmate/backend/common/constants/ApiMappingPattern.java
@@ -1,6 +1,11 @@
 package com.bcl.fitmate.backend.common.constants;
 
 public interface ApiMappingPattern {
+    String AUTH_API = "/api/v2/auth";
+    String ADMIN_API = "/api/v2/admins";
+    String USER_API = "/api/v2/users";
     String COMMON_API = "/api/v2/common";
-
+    String MEMBER_ONE_DAY_TICKET_API = MEMBER_API + "/me/one-day-tickets";
+    String BOARD_COMMENT_API = BOARD_API + "/{boardId}/comments";
+    String FILE_API = "/api/v2/files";
 }
