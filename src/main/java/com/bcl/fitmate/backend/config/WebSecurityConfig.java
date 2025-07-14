e993be1d09787bf35476a74e62935d4d93c075fc
leejunwoo24 <yusm8758@naver.com>
2025-07-14 16:43:41 +0900

    refactor:coupon-note

--- src/main/java/com/bcl/fitmate/backend/config/WebSecurityConfig.java
+++ src/main/java/com/bcl/fitmate/backend/config/WebSecurityConfig.java
@@ -58,7 +58,6 @@ public class WebSecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v2/auth/**").permitAll()
-                        .requestMatchers("/files/**").permitAll()
                         .requestMatchers("/api/v2/files/**").permitAll()
                         .requestMatchers("/api/v2/members/**").hasRole("MEMBER")
                         .requestMatchers("/api/v2/trainers/**").hasRole("TRAINER")
