822c9cc26dff1105b0a508df02e414f56afd1e67
leejunwoo24 <yusm8758@naver.com>
2025-07-15 12:21:03 +0900

    refactor: coupon

--- src/main/java/com/bcl/fitmate/backend/dto/coupon/request/PutCouponRequestDto.java
+++ src/main/java/com/bcl/fitmate/backend/dto/coupon/request/PutCouponRequestDto.java
@@ -2,9 +2,13 @@ package com.bcl.fitmate.backend.dto.coupon.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PutCouponRequestDto {
     @NotBlank
     @JsonFormat(pattern = "yyyy-MM-dd")
