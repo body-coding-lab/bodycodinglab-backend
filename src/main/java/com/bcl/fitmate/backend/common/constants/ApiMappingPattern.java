7514cb4c38b26d2d6bd3cd74c9096f17114fcda9
bbang-2 <yys799@nate.com>
2025-07-17 16:27:34 +0900

    refactor: statistic api 리팩토링

--- src/main/java/com/bcl/fitmate/backend/common/constants/ApiMappingPattern.java
+++ src/main/java/com/bcl/fitmate/backend/common/constants/ApiMappingPattern.java
@@ -22,5 +22,5 @@ public interface ApiMappingPattern {
     String TRAINER_MATCH_API = TRAINER_API + "/me/match-list";
     String MEMBER_SUBSCRIPTION_API = MEMBER_API + "/me/subscription";
     String PAYMENT_API = MEMBER_API + "/me/payment";
-    String STATISTIC_API = "/api/v2/statistic";
+    String STATISTIC_API = "/api/v2/statistics";
 }
